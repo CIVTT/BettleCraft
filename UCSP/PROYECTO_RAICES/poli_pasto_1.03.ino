9b393f6bcdb9a6b5f2e2fcc957c437e2bb09cda2
CIVTT <hvictorh.p@gmail.com>
2023-11-10 17:37:35 -0500

    REUBICADO

--- UCSP/PROYECTO_RAICES/poli_pasto_1.03.ino
+++ UCSP/PROYECTO_RAICES/poli_pasto_1.03.ino
@@ -26,17 +26,18 @@ const unsigned long periodo=1000;
 unsigned long perante=0;
 char caracter;
 float SETFREC = 0;
+float DL=0;
 String dat_1 = "";
-boolean running = false;
+int running = LOW;
 SoftwareSerial puerta(rxPin, txPin);
 ModbusRTUMaster modbus(puerta, dePin);
 LiquidCrystal_I2C lcd_1(0x27, 20, 4);
 float valor_galga = 0;
-int valor_in = 0;
 float vel = 0;
 float rpm = 0;
 File my_File;
 String onoff = "OFF";
+int variador_frec=0;
 
 void star() {
   //Serial.println(F("mandando"));
@@ -46,7 +47,7 @@ void star() {
   if (modbus.writeSingleHoldingRegister(01, 8192, value)) {
     //Serial.println(F("ENCENDIDO"));
   }  //else processError();
-  running = true;
+  running = HIGH;
   onoff = "ON";
 }
 void stop() {
@@ -56,7 +57,7 @@ void stop() {
   uint16_t value = 7;
   if (modbus.writeSingleHoldingRegister(01, 8192, value)) {
   }  // else processError();
-  running = false;
+  running = LOW;
   onoff = "OFF";
 }
 void envio(uint16_t value) {
@@ -66,6 +67,18 @@ void envio(uint16_t value) {
     //valor_in = value;
   }
   //else processError();
+}
+
+void frec_var() {
+  uint16_t value = 0;
+  //long val_1;
+  //float val;
+  if (modbus.readHoldingRegisters(01, 8451, &value, 1)) {
+      variador_frec=value;
+  }
+  //else //processError();
+  //return val;
+  
 }
 void ini_sd() {
 
@@ -109,19 +122,16 @@ void setup() {
   lcd_1.clear();
   lcd_1.setCursor(0, 0);
   lcd_1.print("Iniciando espere...");
-  delay(1000);
+  delay(2000);
   lcd_1.clear();
   lcd_1.setCursor(0, 0);
   lcd_1.print("------UCSP------");
   lcd_1.setCursor(0, 1);
-  lcd_1.print("PROYEC POLIPASTO");
-  delay(1000);
+  lcd_1.print("PROYECTO  RAICES");
+  delay(2000);
 }
 void loop() {
-
-  calculate();
-  display();
-
+  
   if (Serial.available()) {
     caracter = Serial.read();
     switch (caracter) {
@@ -200,20 +210,20 @@ void loop() {
       pulsado_2 = estado_2;
     }
   }
-
-  if (running) {
-    unsigned long peri_act=millis();
-    if(peri_act-perante>=periodo){
-    cont = cont + 1;
-    //while (millis() % 1000 != 0) {}
+calculate();
+unsigned long peri_act=millis();
+if(peri_act-perante >= periodo){
+  
+  display();
+  frec_var();
+  
+  if (valor_galga > 1 && running == HIGH) {
+    
     sd_card();
-    perante=peri_act;
-    }
-  } else {
-    cont = 0;
-    //my_File.close();
-  }
-}
+    cont = cont + 1;
+    } else {cont = 0;}
+  perante=peri_act;
+}}
 void display() {
   lcd_1.clear();
   lcd_1.setCursor(0, 0);
@@ -233,25 +243,28 @@ void display() {
   lcd_1.print("PWR:");
   lcd_1.setCursor(13, 1);
   lcd_1.print(onoff);
-  while (millis() % 300 != 0) {}
+  //while (millis() % 250 != 0) {}
 }
 void calculate() {
   //velocidad en mm/s
-
   SETFREC = float(frecc[con_fre]) / 100;
-  //SETFREC = SETFREC / 100; //posible cambio si falla por interferencia en variador y serial, comentar y descomenrtar anterior linea
   rpm = (SETFREC * 0.741) - 1.43;
   if (rpm <= 0) {
     rpm = 0;
-    vel = 0;
+    //vel = 0;
   }
-  if (rpm > 0) {
-    vel = rpm * pi * 80 / 30;
+  /*if (rpm > 0) {
+    vel = rpm * pi * 135 / 60;
+  }*/
+  vel=((float(variador_frec)/100)*0.741 - 1.43) *pi*135/60;
+  if(vel<0){
+    vel=0;
   }
   //peso
   adc_raw = analogRead(sensorPin);
   adc_filtrado = (alpha * adc_raw) + ((1 - alpha) * adc_filtrado);
   valor_galga = (500 * adc_filtrado) / 1023;
+  DL=cont*vel;
   //dat_1 = String(cont) + ";" + String(rpm, 2) + ";" + String(vel, 2) + ";" + String(valor_galga, 2);
 }
 
@@ -291,8 +304,10 @@ void sd_card() {
     my_File.print(";");
     my_File.print(vel);
     my_File.print(";");
-    my_File.println(valor_galga);
-     // my_File.println(dat_1);
+    my_File.print(valor_galga);
+    my_File.print(";");
+    my_File.println(DL);
+    // my_File.println(dat_1);
       //Serial.println("Escribiendo SD: ");
       //delay(10);
       my_File.close();  //cerramos el archivo
